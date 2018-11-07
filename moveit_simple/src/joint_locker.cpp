fb9f12a73672faef3f45471eac98ad5ec973473d
geoffreychiou <geoffrey.chiou@plusonerobotics.com>
2018-11-07 12:19:01 -0600

    Replace lockJoints and resolveToString with @joshuaplusone's suggestions

--- moveit_simple/src/joint_locker.cpp
+++ moveit_simple/src/joint_locker.cpp
@@ -23,92 +23,40 @@ namespace moveit_simple
 
 void JointLocker::lockJoints(const std::vector<double> &prev_joints, std::vector<double> &current_joints, JointLockOptions options)
 {
-  if ((options & JointLockOptions::LOCK_JOINT_1) != JointLockOptions::LOCK_NONE)
+  for (auto i = 1UL; i < sizeof(JointLockOptions); i++)
   {
-    current_joints[0] = prev_joints[0];
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_2) != JointLockOptions::LOCK_NONE)
-  {
-    current_joints[1] = prev_joints[1];
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_3) != JointLockOptions::LOCK_NONE)
-  {
-    current_joints[2] = prev_joints[2];
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_4) != JointLockOptions::LOCK_NONE)
-  {
-    current_joints[3] = prev_joints[3];
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_5) != JointLockOptions::LOCK_NONE)
-  {
-    current_joints[4] = prev_joints[4];
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_6) != JointLockOptions::LOCK_NONE)
-  {
-    current_joints[5] = prev_joints[5];
+    if ((static_cast<uchar_t>(options) & (1 << i)) != 0)
+    {
+      current_joints[i - 1] = prev_joints[i - 1];
+    }
   }
 }
 
 std::string JointLocker::resolveToString(const JointLockOptions options)
 {
-  std::vector<std::string> locked_joints;
+  std::string result;
+  const std::string prefix = "LOCK_JOINT_";
 
-  if ((options & JointLockOptions::LOCK_JOINT_1) != JointLockOptions::LOCK_NONE)
+  for (auto i = 1UL; i < sizeof(options); i++)
   {
-    locked_joints.push_back("LOCK_JOINT_1");
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_2) != JointLockOptions::LOCK_NONE)
-  {
-    locked_joints.push_back("LOCK_JOINT_2");
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_3) != JointLockOptions::LOCK_NONE)
-  {
-    locked_joints.push_back("LOCK_JOINT_3");
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_4) != JointLockOptions::LOCK_NONE)
-  {
-    locked_joints.push_back("LOCK_JOINT_4");
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_5) != JointLockOptions::LOCK_NONE)
-  {
-    locked_joints.push_back("LOCK_JOINT_5");
-  }
-
-  if ((options & JointLockOptions::LOCK_JOINT_6) != JointLockOptions::LOCK_NONE)
-  {
-    locked_joints.push_back("LOCK_JOINT_6");
-  }
-
-  if (locked_joints.size() == 0)
-  {
-    return std::string("LOCK_NONE");
-  }
-  else
-  {
-    std::string locked_joints_str;
-    for (size_t i = 0; i < locked_joints.size(); ++i)
+    if ((static_cast<uchar_t>(options) & (1 << i)) != 0)
     {
-      if (i == 0)
+      auto joint_string = prefix + std::to_string(i);
+      if (!result.empty())
       {
-        locked_joints_str += locked_joints[i];
+        result += ", ";
       }
-      else
-      {
-        locked_joints_str += ", " + locked_joints[i];
-      }
-    }
 
-    return locked_joints_str;
+      result += joint_string;
+    }
   }
+
+  if (result == "")
+  {
+    return "LOCK_NONE";
+  }
+
+  return result;
 }
 
 } // namespace moveit_simple
